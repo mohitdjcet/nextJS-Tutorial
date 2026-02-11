0baf3ccbb724829a54230d183317b79ab9fcc1ed
Mohit Kumar <mks.djcet@gmail.com>
2026-02-11 16:04:39 +0530

    first commit

---
+++ app/contact/page.jsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+    return(
+        <div>
+            <h1>Contact Page</h1>
+            <p>This is the Contact page of our Next.js application.</p>
+        </div>
+    )
+}

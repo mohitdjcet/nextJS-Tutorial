4249c4ef85637bff3b43a61de624013795cbb329
Mohit Kumar <mks.djcet@gmail.com>
2026-02-19 14:59:48 +0530

    Updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,15 @@
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-}
-
 export default async function Home() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts",{
-    // cache: "no-store",
-    next:{revalidate: 1}
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch posts");
-  }
-
-  const posts: Post[] = await res.json();
-
-  return (
-    <div>
-      <h1>Posts Fetch Data by Server</h1>
-      <ul>
-        {posts.slice(0, 5).map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1',{
+        // cache: 'no-store'
+        next: {revalidate: 10}
+    })
+    const data = await res.json()
+    console.log(data)
+    return (
+        <div>
+            <h1>Welcome to the Home Page(Dynamic Render Page)</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    )
 }

9dc5cbd28b280f1629812209b59a988f6da981d4
Mohit Kumar <mks.djcet@gmail.com>
2026-03-22 09:38:59 +0530

    Updated code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import type { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -10,14 +9,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <header style={{padding:12, background:"#a69e9e"}}>
           My App Navbar
-          <nav style={{display:"flex", gap:12}}>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/dashboard">DashBoard</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/products">Products</Link>
-          </nav>
           </header>
         {children}
         <footer style={{padding:12, background:"#a69e9e"}}>My Footer</footer>

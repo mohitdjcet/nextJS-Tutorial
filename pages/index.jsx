ce743dd06893bda8f9f1df467c1e4ed2171eb698
Mohit Kumar <mks.djcet@gmail.com>
2026-03-25 15:11:00 +0530

    Updated code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,47 @@
-import styles from "@/styles/Home.module.css";
+// import { useState,useEffect } from "react"
 
-export default function Home(){
-    return(
-        <div>
-            <h1 style={{color:"green"}}>Home Page</h1>
-            <button className={styles.button}>Click me</button>
-        </div>
-    )
+// export default function Home(){
+//     const [users,setUsers]= useState([]);
+
+//     useEffect(()=>{
+//         async function fetchData(){
+//             const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//             const data = await res.json();
+//             setUsers(data)
+//         }
+//         fetchData();
+//     },[])
+//     return(
+//         <div>
+//             <h1>User Lists (CSR)</h1>
+//             {
+//                 users.map((user)=>(
+//                     <p key={user.id}>{user.name}</p>
+//                 ))
+//             }
+//         </div>
+//     )
+// }
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      users: data,
+    },
+    revalidate: 10, //ISR
+  };
+}
+
+export default function Home({ users }) {
+  return (
+    <div>
+      <h1>User Lists (SSG+ISR)</h1>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
 }

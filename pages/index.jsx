3008020c4cad0d23dde70dfb5d670c8b4d944c53
Mohit Kumar <mks.djcet@gmail.com>
2026-03-25 14:38:06 +0530

    Updated code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,10 @@
+import styles from "@/styles/Home.module.css";
+
 export default function Home(){
-    return <h1>Home Page</h1>
+    return(
+        <div>
+            <h1 style={{color:"green"}}>Home Page</h1>
+            <button className={styles.button}>Click me</button>
+        </div>
+    )
 }
